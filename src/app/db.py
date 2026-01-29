06885b2491adafbe92ede45bb6e572e47eacab91
Peter Grivalsky <pp.grivalsky@gmail.com>
2026-01-29 09:28:04 +0100

    feat: initialize database and create general table

---
+++ src/app/db.py
@@ -0,0 +1,34 @@
+import sqlite3
+from pathlib import Path
+
+DB_PATH = Path("data") / "book_manager.db"
+
+def get_connection() -> sqlite3.Connection:
+    DB_PATH.parent.mkdir(parents=True, exist_ok=True)
+    return sqlite3.connect(DB_PATH)
+
+def create_tables() -> None:
+    query =  """
+    CREATE TABLE IF NOT EXISTS books (
+    id integer PRIMARY KEY, 
+    title text NOT NULL, 
+    author text NOT NULL, 
+    year integer NOT NULL,
+    rating integer CHECK (rating BETWEEN 1 AND 5)
+    );
+    """
+    conn = get_connection()
+    try:
+        conn.execute(query)
+        conn.commit()
+    finally: 
+        conn.close()
+
+
+def main():
+    create_tables()
+    print("Table was created!")
+
+
+if __name__ == "__main__":
+    main()
